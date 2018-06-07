b7cd54664b6a6b87b3f2cd04cd500db7c93ff2c5
shadab14meb346 <31199288+shadab14meb346@users.noreply.github.com>
2018-06-08 02:29:01 +0530

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -43,16 +43,17 @@
     });
 
 var _hash;
- $("#push").click(function(){
-  n.files.add(new n.types.Buffer($("#string").val()), (err, filesAdded) => {
+function HashFunction(){
+   n.files.add(new n.types.Buffer($("#string").val()), (err, filesAdded) => {
       if (err) {
         return console.error('Error - ipfs files add', err, res)
       }
 
-      filesAdded.forEach((file) => 
-        _hash= file.hash);
- });
-  $("#hash").text(_hash);
+      filesAdded.forEach((file) => _hash= file.hash);
+ }); 
+$("#hash").text(_hash);
+}
+ $("#push").click(function(){
   contractInst.sendHash(_hash,(err,res)=>{
   if(err)
     console.log(err);
@@ -61,17 +62,18 @@ var _hash;
   $("#tran").text(_hash);
 });
   });
-
-
-
+var blockchainHash;
  $("#get").click(function(){
   contractInst.getHash((err,res)=>{
   if(err)
     console.log(err);
   else
-    n.files.cat(_hash, function (err, data) {
+  console.log(res);
+  blockchainHash=res;
+ });
+    n.files.cat(blockchainHash, function (err, data) {
       if (err) {
-        return console.error('Error - ipfs files cat', err, res)
+        return console.error('Error - ipfs files', err, res)
       }
 
       console.log(data.toString());
